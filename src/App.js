b3f232d4a8ffcd2fd437af43fb40298597a76c2e
Chaerul Umam <chaerulumamb244@gmail.com>
2022-12-06 11:22:03 +0700

    install stacj navigator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {SignIn, SplashScreen} from './pages';
 import {NavigationContainer} from '@react-navigation/native';
+import Router from './router';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SignIn />
-      {/* <SplashScreen /> */}
+      <Router />
     </NavigationContainer>
   );
 };
