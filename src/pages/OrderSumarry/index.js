4f4c9d9422e842b12bf72931a18e864ff260b8f2
Chaerul Umam <chaerulumamb244@gmail.com>
2022-12-12 20:41:07 +0700

    modify slicing order summary

--- src/pages/OrderSumarry/index.js
+++ src/pages/OrderSumarry/index.js
@@ -18,7 +18,11 @@ const OrderSummary = () => {
         <ItemValue label="Cherry Healthy" value="IDR 18.390.000" />
         <ItemValue label="Driver" value="IDR 50.000" />
         <ItemValue label="Tax 10%" value="IDR 1.800.390" />
-        <ItemValue label="Total Price" value="IDR 390.803.000" />
+        <ItemValue
+          label="Total Price"
+          value="IDR 390.803.000"
+          colorValue="#1ABC9C"
+        />
       </View>
 
       <View style={styles.content}>
