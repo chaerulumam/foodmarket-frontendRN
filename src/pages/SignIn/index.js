cd2fd003bd67a93a00315f89f6fcf1f86cc1d843
Chaerul Umam <chaerulumamb244@gmail.com>
2022-12-13 22:59:55 +0700

    modify

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -4,8 +4,8 @@ import {Button, Gap, Header, TextInput} from '../../components';
 import {useForm} from '../../utils';
 
 const SignIn = ({navigation}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
 
   const [form, setForm] = useForm({
     email: '',
