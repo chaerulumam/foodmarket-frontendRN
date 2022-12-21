8ef5f4419be9954c4825a5cad432e1c83713c64a
Chaerul Umam <chaerulumamb244@gmail.com>
2022-12-22 00:15:32 +0700

    implement react native image picker

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,16 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from 'react-native';
 import {Button, Gap, Header, TextInput} from '../../components';
-import {useSelector, useDispatch} from 'react-redux';
-import {useForm} from '../../utils';
+import {useDispatch} from 'react-redux';
+import {showMessage, useForm} from '../../utils';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 const SignUp = ({navigation}) => {
   const [form, setForm] = useForm({
@@ -11,49 +19,93 @@ const SignUp = ({navigation}) => {
     password: '',
   });
 
+  const [photo, setPhoto] = useState('');
+
   const dispatch = useDispatch();
 
   const onSubmit = () => {
-    console.log('form: ', form);
+    console.log('form', form);
     dispatch({type: 'SET_REGISTER', value: form});
     navigation.navigate('SignUpAddress');
   };
 
+  const addPhoto = () => {
+    launchImageLibrary(
+      {
+        quality: 0.5,
+        maxWidth: 200,
+        maxHeight: 200,
+      },
+      response => {
+        console.log('Response = ', response);
+
+        if (response.didCancel || response.error) {
+          showMessage('Anda Tidak Memilih Photo');
+        } else {
+          const source = {uri: response.uri};
+          const dataImage = {
+            uri: response.uri,
+            type: response.type,
+            name: response.fileName,
+          };
+
+          setPhoto(source);
+          dispatch({type: 'SET_PHOTO', value: dataImage});
+          dispatch({type: 'SET_UPLOAD_STATUS', value: true});
+          console.log('response');
+        }
+      },
+    );
+  };
+
   return (
-    <ScrollView contentContainerStyle={{flexGrow: 1}}>
+    <ScrollView contentContainerStyle={styles.scroll}>
       <View style={styles.page}>
-        <Header title="Sign Up" subTitle="Register and eat" back={() => {}} />
+        <Header
+          title="Sign Up"
+          subTitle="Register And Eat"
+          onBack={() => navigation.goBack()}
+        />
         <View style={styles.container}>
           <View style={styles.photo}>
-            <View style={styles.borderPhoto}>
-              <View style={styles.photoContainer}>
-                <Text style={styles.addPhoto}>Add Photo</Text>
+            <TouchableOpacity onPress={addPhoto}>
+              <View style={styles.borderPhoto}>
+                {photo ? (
+                  <Image source={photo} style={styles.photoContainer} />
+                ) : (
+                  <View style={styles.photoContainer}>
+                    <Text style={styles.addPhoto}>Add Photo</Text>
+                  </View>
+                )}
               </View>
-            </View>
+            </TouchableOpacity>
           </View>
+
           <TextInput
             label="Full Name"
-            placeholder="Type your full name"
+            placeholder="Type Your Full Name"
             value={form.name}
             onChangeText={value => setForm('name', value)}
           />
           <Gap height={16} />
+
           <TextInput
             label="Email Address"
-            placeholder="Type your email address"
+            placeholder="Type Your Email Address"
             value={form.email}
             onChangeText={value => setForm('email', value)}
           />
           <Gap height={16} />
+
           <TextInput
             label="Password"
-            placeholder="Type your passwords"
-            secureTextEntry
+            placeholder="Type Your Password"
             value={form.password}
             onChangeText={value => setForm('password', value)}
+            secureTextEntry
           />
-          <Gap height={24} />
 
+          <Gap height={24} />
           <Button text="Continue" onPress={onSubmit} />
         </View>
       </View>
@@ -64,7 +116,12 @@ const SignUp = ({navigation}) => {
 export default SignUp;
 
 const styles = StyleSheet.create({
+  scroll: {
+    flexGrow: 1,
+  },
+
   page: {flex: 1},
+
   container: {
     backgroundColor: 'white',
     paddingHorizontal: 24,
@@ -72,7 +129,13 @@ const styles = StyleSheet.create({
     marginTop: 24,
     flex: 1,
   },
-  photo: {alignItems: 'center', marginTop: 26, marginBottom: 16},
+
+  photo: {
+    alignItems: 'center',
+    marginTop: 26,
+    marginBottom: 16,
+  },
+
   borderPhoto: {
     borderWidth: 1,
     borderColor: '#8D92A3',
@@ -83,13 +146,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+
   photoContainer: {
     width: 90,
     height: 90,
     borderRadius: 90,
     backgroundColor: '#F0F0F0',
-    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+
   addPhoto: {
     fontSize: 14,
     fontFamily: 'Poppins-Light',
