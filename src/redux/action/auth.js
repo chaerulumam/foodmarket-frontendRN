b979c151b8497fe10057a5e3fd360475efede31e
Chaerul Umam <chaerulumamb244@gmail.com>
2022-12-26 19:54:22 +0700

    install & setup asyncstorage

--- src/redux/action/auth.js
+++ src/redux/action/auth.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import {showMessage} from '../../utils';
+import {showMessage, storeData} from '../../utils';
 import {setLoading} from './global';
 
 const API_HOST = {
@@ -10,7 +10,13 @@ export const signUpAction =
   (dataRegister, photoReducer, navigation) => dispatch => {
     Axios.post(`${API_HOST.url}/register`, dataRegister)
       .then(res => {
-        console.log('data success: ', res.data);
+        const token = `${res.data.data.token_type} ${res.data.data.access_token}`;
+        const profile = res.data.data.user;
+
+        // data user
+        storeData('userProfile', profile);
+        // data token
+        storeData('token', {value: token});
 
         if (photoReducer.isUploadPhoto) {
           const photoForUpload = new FormData();
@@ -18,19 +24,15 @@ export const signUpAction =
 
           Axios.post(`${API_HOST.url}/user/photo`, photoForUpload, {
             headers: {
-              Authorization: `${res.data.data.token_type} ${res.data.data.access_token}`,
+              Authorization: token,
               'Content-Type': 'multipart/form-data',
             },
-          })
-            .then(resUpload => {
-              console.log('success upload', resUpload);
-            })
-            .catch(err => {
-              showMessage('Upload photo tidak berhasil');
-            });
+          }).catch(err => {
+            showMessage('Upload photo tidak berhasil');
+          });
         }
         dispatch(setLoading(false));
-        showMessage('Register Success', 'success');
+
         navigation.replace('SignUpSuccess');
       })
       .catch(err => {
